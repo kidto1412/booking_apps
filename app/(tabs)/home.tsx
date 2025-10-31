661091686cca3765f87655228cd38bc73927fbe6
fahriz.dimasqy <fahriz.dimasqy@dwidasa.com>
2025-11-01 00:18:14 +0700

    added component

---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function HomePage() {
+  return (
+    <SafeAreaView>
+      <Button>Hello</Button>
+    </SafeAreaView>
+  );
+}
